beb2718e46aacebe60bafb93ce59297bb19f5d95
Noman73 <noman.eng73@gmail.com>
2022-12-13 10:39:38 +0600

    13-12-2022

--- resources/views/backend/view_pages/invoices/total_chalan.blade.php
+++ resources/views/backend/view_pages/invoices/total_chalan.blade.php
@@ -116,8 +116,8 @@
                   <table class="table table-bordered table-striped">
                       <thead>
                           <tr>
-                              <th width="30%" class="text-center">SL.</th>
-                              <th width="50%">Product Name</th>
+                              <th width="10%" class="text-center">SL.</th>
+                              <th width="70%">Product Name</th>
                               <th width="20%" class="text-right">Quantity</th>
                           </tr>
                       </thead>
