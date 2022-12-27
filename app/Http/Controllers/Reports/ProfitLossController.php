3d1482cbf8b1e0ed648cc18df6f1a6f2b65e8612
Noman73 <noman.eng73@gmail.com>
2022-12-27 17:29:23 +0600

    27-12-2022

--- app/Http/Controllers/Reports/ProfitLossController.php
+++ app/Http/Controllers/Reports/ProfitLossController.php
@@ -92,9 +92,10 @@ class ProfitLossController extends Controller
 
         $expenses=DB::select("
         select account_ledgers.name,ifnull(sum(voucers.debit-voucers.credit),0.00) total from account_ledgers
-        inner join account_groups on account_groups.id=account_ledgers.group_id and account_groups.name='Indirect Expenses'
+        inner join account_groups on account_groups.id=account_ledgers.group_id 
         left join voucers on voucers.ledger_id=account_ledgers.id
         and voucers.date>=:from_date and voucers.date<=:to_date
+        where account_groups.name='Indirect Expenses'
         group by account_ledgers.id
         ",['from_date'=>$from_date,'to_date'=>$to_date]);
 
