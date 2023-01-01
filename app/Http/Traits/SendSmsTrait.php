941fbf110d996c51985a1d1fba4e2dd0f68dd1e6
Noman73 <noman.eng73@gmail.com>
2023-01-01 10:45:06 +0600

    01-01-2023

--- app/Http/Traits/SendSmsTrait.php
+++ app/Http/Traits/SendSmsTrait.php
@@ -6,28 +6,25 @@ trait SendSmsTrait{
 
 	public function sendSms($sms,$number)
 	{
-        info($sms);
-        $api_key="HOB2tfILEQWzSumTbiG8Gozihzaf52Ok";
-        $api_token="Sagc1672290805";
-        $sender_id="SMR Agro";
-        $contacts='88'.$number;
-        $type="unicode";
-        $msg=$sms;
-        $fields='sendsms&apikey='.$api_key.'&apitoken='.$api_token.'&type='.$type.'&from='.$sender_id.'&to='.$contacts.'&text='.$msg;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://app.mimsms.com/smsAPI");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
-        // In real life you should use something like:
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, 
-        //          http_build_query(array('postvar1' => 'value1')));
-        // Receive server response ...
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $server_output = curl_exec($ch);
-        curl_close ($ch);
-        // Further processing ...
-        info($server_output);
-        return $server_output;
+                $api_key="C2001593632a9d8ed9db24.24710771";
+                $sender_id="8809601003570";
+                $contacts=$number;
+                $type="application/json";
+                $msg=$sms;
+                $fields='api_key='.$api_key.'&type='.$type.'&contacts='.$contacts.'&senderid='.$sender_id.'&msg='.$msg;
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,"https://isms.mimsms.com/smsapi");
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
+                // In real life you should use something like:
+                // curl_setopt($ch, CURLOPT_POSTFIELDS, 
+                //          http_build_query(array('postvar1' => 'value1')));
+                // Receive server response ...
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                curl_close ($ch);
+                // Further processing ...
+                return $server_output;
 	}
 
 }
